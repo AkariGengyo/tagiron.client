7fc12df0aa226f015ba1a89f51e55c40c373f36a
AkariGengyo <akari.3688@gmail.com>
2022-08-02 17:38:45 +0900

    数字タイルコンポーネントの実装

---
+++ src/components/parts/numberTile.tsx
@@ -0,0 +1,31 @@
+type Props = {
+  children: string;
+  color: string;
+};
+
+/**
+ * 数字カードコンポーネント
+ * @param children 内容
+ */
+
+const NumberTile: React.VFC<Props> = ({ children, color }) => {
+  let cardColor;
+  if (color == "red") {
+    cardColor = "bg-red-500";
+  } else if (color == "blue") {
+    cardColor = "bg-blue-500";
+  } else if (color == "yellow") {
+    cardColor = "bg-yellow-300";
+  }
+  return (
+    <div className="flex bg-black border-2 border-white rounded w-12 h-14 justify-center items-center">
+      <div
+        className={`flex rounded-full w-10 h-10 justify-center items-center ${cardColor}`}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default NumberTile;
