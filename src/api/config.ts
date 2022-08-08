1d1f7b074d827ede4798a021952f1085f53a6c4a
Jun-T-git <jun0303t@icloud.com>
2022-08-08 16:47:59 +0900

    デフォルトport変更

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,7 +2,7 @@ import axiosBase from "axios";
 
 // APIのURL
 export const apiBaseUrl =
-  process.env.NEXT_PUBLIC_API_BASEURL || "http://localhost:8000";
+  process.env.NEXT_PUBLIC_API_BASEURL || "http://localhost:3000/api";
 
 // axiosの基本設定
 export const axios = axiosBase.create({
